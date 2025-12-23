4d88e94d4280b0ec55758d78ac87a55159aba6be
LelouchEmperouge0 <divinagbenohevi@gmail.com>
2025-12-23 22:31:46 +0000

    fix(render): use npx to avoid vite permission error

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -17,7 +17,7 @@
       />
       <button @click="submitSearch" class="ml-1 text-gray-500 hover:text-gray-700">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-6-6m6 6V9m0 0H9m6 6h6" />
+          
         </svg>
       </button>
     </div>
